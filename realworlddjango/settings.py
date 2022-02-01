b4562545f3622fc383ae3c362652da48f8246b91
Dmitry Kobzarenko <kobzarenko_dm@mail.ru>
2022-02-01 18:20:56 +0300

    Heroku Initial

--- realworlddjango/settings.py
+++ realworlddjango/settings.py
@@ -12,16 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 #import environ
 from pathlib import Path
-import os
-import django_heroku
-django_heroku.settings(locals())
 
 #env = environ.Env()
 #environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -33,7 +30,6 @@ SECRET_KEY = '**************************'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -123,16 +119,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_URL = 'static/'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+import django_heroku
+django_heroku.settings(locals())
