243abeea4cfbac375d68c3f531acb4fd47554841
Dmitry Kobzarenko <kobzarenko_dm@mail.ru>
2022-02-01 13:52:41 +0300

    Heroky Initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: realworlddjango.wsgi --log-file -
---
+++ events/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# модель Event
+class Event(models.Model):
+    title = models.CharField(max_length=200, default='',
+                             verbose_name='Название')
+    description = models.TextField(default='',
+                                   verbose_name='Описание')
+    date_start = models.DateTimeField(verbose_name='Дата начала')
+    participants_number = models.PositiveIntegerField(
+        validators = [MinValueValidator(1), MaxValueValidator(10000)],
+        verbose_name='Количество участников')
+    is_private = models.BooleanField(default=False, verbose_name='Частное')
+
+    class Meta:
+        verbose_name_plural = 'События'  # форма единственного числа
+        verbose_name = 'Событие'  # форма множественного числа
+
+    def __str__(self):
+        return self.title
+
+
+# модель Category
+class Category(models.Model):
+    title = models.CharField(max_length=200, default='',
+                             verbose_name='Название')
+
+    class Meta:
+        verbose_name_plural = 'Категории'  # форма единственного числа
+        verbose_name = 'Категория'  # форма множественного числа
+
+    def __str__(self):
+        return self.title
