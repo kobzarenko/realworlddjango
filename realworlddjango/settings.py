9517ea82edf4a1eac493aa2611e13a3036132279
Дмитрий Кобзаренко <54996525+kobzarenko@users.noreply.github.com>
2022-02-01 15:40:29 +0300

    Update settings.py

--- realworlddjango/settings.py
+++ realworlddjango/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 #import environ
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
 
 #env = environ.Env()
 #environ.Env.read_env()
@@ -26,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = '************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
